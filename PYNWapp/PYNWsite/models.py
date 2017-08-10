f0e7e3d5f08e10e3d88f98c1353509681893e6d1
Adam <adamlives@hotmail.com>
2017-08-10 23:01:28 +0100

    added admin and activated models

--- PYNWapp/PYNWsite/models.py
+++ PYNWapp/PYNWsite/models.py
@@ -17,3 +17,14 @@ class Event(models.Model):
 
     def is_future(self):
        return self.event_date > timezone.now()
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    body = models.TextField()
+    posted = models.DateField(db_index=True, auto_now_add=True)
+    category = models.ForeignKey('Category')
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
--- PYNWapp/PYNWsite/views.py
+++ PYNWapp/PYNWsite/views.py
@@ -4,8 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Event
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World. You're at the PYNW index.")
+    event_list = Event.objects.order_by('event_date')[:5]
+    context = {'event_list': event_list}
+    return render(request, 'PYNWsite/index.html', context)
+#    return HttpResponse("Hello, World. You're at the PYNW index.")
 
 
