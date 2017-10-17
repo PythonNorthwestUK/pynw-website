0c92bee5a5b3000d24388c5e45b11c9f8d0d4b4d
Adam <adamlives@hotmail.com>
2017-10-17 20:33:32 +0100

    test

--- PYNWapp/PYNWapp/urls.py
+++ PYNWapp/PYNWapp/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('PYNWsite.urls'))
+    url(r'', include('PYNWsite.urls'),
+    url(r'event/([0-9])/', include('PYNWsite.urls'))
 ]
